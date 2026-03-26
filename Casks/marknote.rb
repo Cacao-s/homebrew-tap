dbd8121c6546754f908b98d8294994dac63713a8
Ricky <zeyanlin@outlook.com>
2026-03-26 13:27:27 +0800

    chore: bump version to 0.7.0

--- Casks/marknote.rb
+++ Casks/marknote.rb
@@ -1,6 +1,6 @@
 cask "marknote" do
-  version "0.6.0"
-  sha256 "3c90e7d47de226aaa68ba44eff3eba977e688c4dcce4851d304efcdad5d64083"
+  version "0.7.0"
+  sha256 "5a30315b5e539ef0e5e22075dee064e1ec22bb972b34d26fc9f7869410d467a3"
 
   url "https://github.com/Cacao-s/marknote-official/releases/download/v#{version}/MarkNote_#{version}_universal.dmg"
   name "marknote"
